4b4c76fe29f5007dbdc1e45180ba0e57c9dd34c9
lightmon <lightmon5210@outlook.com>
2024-10-10 14:40:45 +0800

    tmp

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,9 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    // extraResource: [
+    //   "Makefile"
+    // ]
   },
   rebuildConfig: {},
   makers: [
